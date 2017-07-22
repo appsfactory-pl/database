42fa555901090956b480d4a346f43dc97751ac3d
pawelkolodziejuk <pawelkolodziejuk@mbp-pawel.home>
2017-07-22 15:51:45 +0200

    biblioteki

--- src/AppBundle/Controller/LibraryController.php
+++ src/AppBundle/Controller/LibraryController.php
@@ -5,129 +5,173 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Doctrine\ORM\EntityManagerInterface;
+use AppBundle\Form\MaritialStatusType;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Form\FileTypeType;
 
-class LibraryController extends Controller
-{
+class LibraryController extends Controller {
+
     /**
      * @Route("/library/maritial-status")
      */
-    public function maritialStatusAction(EntityManagerInterface $em)
-    {
+    public function maritialStatusAction(EntityManagerInterface $em) {
         $repository = $em->getRepository('AppBundle:MaritialStatus');
         $statuses = $repository->findAll();
         return $this->render('AppBundle:Library:maritial_status.html.twig', array(
-            // ...
-            'statuses' => $statuses,
+                    // ...
+                    'statuses' => $statuses,
         ));
     }
 
     /**
      * @Route("/library/maritial-status-add")
      */
-    public function maritialStatusAddAction()
-    {
+    public function maritialStatusAddAction(Request $request) {
+        $form = $this->createForm(MaritialStatusType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $maritialStatus = $form->getData();
+            $em->persist($maritialStatus);
+            $em->flush();
+            return $this->redirectToRoute('app_library_maritialstatus');
+        }
         return $this->render('AppBundle:Library:maritial_status_add.html.twig', array(
-            // ...
+                    // ...
+                    'form' => $form->createView(),
         ));
     }
 
     /**
      * @Route("/library/maritial-status-edit/{id}")
      */
-    public function maritialStatusEditAction($id)
-    {
+    public function maritialStatusEditAction(Request $request, $id) {
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository('AppBundle:MaritialStatus');
+        $status = $repo->find($id);
+        if (empty($status)) {
+            $this->redirectToRoute('app_library_maritialstatus');
+        }
+        $form = $this->createForm(MaritialStatusType::class, $status);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $maritialStatus = $form->getData();
+            $em->persist($maritialStatus);
+            $em->flush();
+            return $this->redirectToRoute('app_library_maritialstatus');
+        }
         return $this->render('AppBundle:Library:maritial_status_edit.html.twig', array(
-            // ...
+                    // ...
+                    'form' => $form->createView(),
         ));
     }
 
     /**
      * @Route("/library/file-type")
      */
-    public function fileTypeAction()
-    {
+    public function fileTypeAction(EntityManagerInterface $em) {
+        $repo = $em->getRepository('AppBundle:FileType');
+        $types = $repo->findAll();
         return $this->render('AppBundle:Library:file_type.html.twig', array(
-            // ...
+                    // ...
+                    'types' => $types,
         ));
     }
 
     /**
      * @Route("/library/file-type-add")
      */
-    public function fileTypeAddAction()
-    {
+    public function fileTypeAddAction(Request $request) {
+        $form = $this->createForm(FileTypeType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $fileType = $form->getData();
+            $em->persist($fileType);
+            $em->flush();
+            return $this->redirectToRoute('app_library_filetype');
+        }
         return $this->render('AppBundle:Library:file_type_add.html.twig', array(
-            // ...
+                    // ...
+                    'form' => $form->createView(),
         ));
     }
 
     /**
      * @Route("/library/file-type-edit/{id}")
      */
-    public function fileTypeEditAction($id)
-    {
+    public function fileTypeEditAction(Request $request, $id) {
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository('AppBundle:FileType');
+        $type = $repo->find($id);
+        if (empty($type)) {
+            $this->redirectToRoute('app_library_filetype');
+        }
+        $form = $this->createForm(FileTypeType::class, $type);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $fileType = $form->getData();
+            $em->persist($fileType);
+            $em->flush();
+            return $this->redirectToRoute('app_library_filetype');
+        }
         return $this->render('AppBundle:Library:file_type_edit.html.twig', array(
-            // ...
+                    // ...
+                    'form' => $form->createView(),
         ));
     }
 
     /**
      * @Route("/library/status")
      */
-    public function statusAction()
-    {
+    public function statusAction() {
         return $this->render('AppBundle:Library:status.html.twig', array(
-            // ...
+                        // ...
         ));
     }
 
     /**
      * @Route("/library/status-add")
      */
-    public function statusAddAction()
-    {
+    public function statusAddAction() {
         return $this->render('AppBundle:Library:status_add.html.twig', array(
-            // ...
+                        // ...
         ));
     }
 
     /**
      * @Route("/library/status-edit/{id}")
      */
-    public function statusEditAction($id)
-    {
+    public function statusEditAction($id) {
         return $this->render('AppBundle:Library:status_edit.html.twig', array(
-            // ...
+                        // ...
         ));
     }
 
     /**
      * @Route("/library/disengegement-reason")
      */
-    public function disengegementReasonAction()
-    {
+    public function disengegementReasonAction() {
         return $this->render('AppBundle:Library:disengegement_reason.html.twig', array(
-            // ...
+                        // ...
         ));
     }
 
     /**
      * @Route("/library/disengegement-reason-add")
      */
-    public function disengegementReasonAddAction()
-    {
+    public function disengegementReasonAddAction() {
         return $this->render('AppBundle:Library:disengegement_reason_add.html.twig', array(
-            // ...
+                        // ...
         ));
     }
 
     /**
      * @Route("/library/disengegement-reason-edit/{id}")
      */
-    public function disengegementReasonEditAction($id)
-    {
+    public function disengegementReasonEditAction($id) {
         return $this->render('AppBundle:Library:disengegement_reason_edit.html.twig', array(
-            // ...
+                        // ...
         ));
     }
 
