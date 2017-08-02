b8944a843fb1b11d894a6555aca5f998008099b6
pawelkolodziejuk <pawelkolodziejuk@mbp-pawel.home>
2017-08-02 07:34:04 +0200

    wyszukiwarka

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -7,46 +7,39 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
-{
+class DefaultController extends Controller {
+
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
-    {
-        if (empty($this->getUser())){
+    public function indexAction(Request $request) {
+        if (empty($this->getUser())) {
             return $this->redirectToRoute('fos_user_security_login');
         }
         $form = $this->createForm(SearchType::class);
         $form->handleRequest($request);
-        if($form->isSubmitted() && $form->isValid()){
+        if ($form->isSubmitted() && $form->isValid()) {
             $em = $this->getDoctrine()->getManager();
             $d = $form->getData();
             $string = $d['string'];
-            if(strlen($string)>2){
+            if (strlen($string) > 2) {
                 $individualRepo = $em->getRepository('AppBundle:Individual');
                 $query = $individualRepo->createQueryBuilder('i')
-//                    ->where('i.id2 = "'.$string.'"')
-                    ->where('i.forename = "jhdsbhugs"')
-            //                $query = $em->createQuery(
-//                    'SELECT i
-//                    FROM AppBundle:Individual
-//                    WHERE i.id2 = :d
-//                    '
-//                )
-//                    ->setParameter('d',$string)
-                    ->getQuery()
-                ;
+                        ->where('i.forename LIKE :data_like')
+                        ->setParameter('data_like','%'.$string.'%')
+                        ->getQuery();
 //                var_dump($query);
                 $individuals = $query->getResult();
+                var_dump($individuals);
             }
 //            $businessRepo = $em->getRepository('AppBundle:Business');
 //            var_dump($d);
         }
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-            'form' => $form->createView(),
+                    'base_dir' => realpath($this->getParameter('kernel.project_dir')) . DIRECTORY_SEPARATOR,
+                    'form' => $form->createView(),
         ]);
     }
+
 }
