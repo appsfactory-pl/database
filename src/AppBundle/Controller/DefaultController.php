ea23621c4bb03516fdc4c43d93d9eb6aea58cebf
vdr <pkolodziejuk@gmail.com>
2017-08-03 12:14:35 +0200

    wyszukiwarka

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -7,15 +7,19 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller {
+class DefaultController extends Controller
+{
 
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request) {
+    public function indexAction(Request $request)
+    {
         if (empty($this->getUser())) {
             return $this->redirectToRoute('fos_user_security_login');
         }
+        $individuals = [];
+        $businesses = [];
         $form = $this->createForm(SearchType::class);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
@@ -25,20 +29,51 @@ class DefaultController extends Controller {
             if (strlen($string) > 2) {
                 $individualRepo = $em->getRepository('AppBundle:Individual');
                 $query = $individualRepo->createQueryBuilder('i')
-                        ->where('i.forename LIKE :data_like')
-                        ->setParameter('data_like','%'.$string.'%')
-                        ->getQuery();
-//                var_dump($query);
+                    ->where('i.forename LIKE :data_like')
+                    ->orWhere('i.id2 = :data_int')
+                    ->orWhere('i.lastname LIKE :data_like')
+                    ->orWhere('i.maidenname LIKE :data_like')
+                    ->orWhere('i.phone LIKE :data_like')
+                    ->orWhere('i.email LIKE :data_like')
+                    ->orWhere('i.address LIKE :data_like')
+                    ->orWhere('i.postcode LIKE :data_like')
+                    ->orWhere('i.nin LIKE :data_like')
+                    ->orWhere('i.utr LIKE :data_like')
+                    ->orWhere('i.bankAccountDetails LIKE :data_like')
+                    ->orWhere('i.notes LIKE :data_like')
+                    ->orWhere('i.archiveNote LIKE :data_like')
+                    ->setParameter('data_like', '%' . $string . '%')
+                    ->setParameter('data_int', (int)$string)
+                    ->getQuery();
                 $individuals = $query->getResult();
-                var_dump($individuals);
+                $businessRepo = $em->getRepository('AppBundle:Business');
+                $query = $businessRepo->createQueryBuilder('b')
+                    ->where('b.id2 = :data_int')
+                    ->orWhere('b.name LIKE :data_like')
+                    ->orWhere('b.cno LIKE :data_like')
+                    ->orWhere('b.telephone LIKE :data_like')
+                    ->orWhere('b.email LIKE :data_like')
+                    ->orWhere('b.address LIKE :data_like')
+                    ->orWhere('b.postcode LIKE :data_like')
+                    ->orWhere('b.utr LIKE :data_like')
+                    ->orWhere('b.vat LIKE :data_like')
+                    ->orWhere('b.epaye LIKE :data_like')
+                    ->orWhere('b.accoff LIKE :data_like')
+                    ->orWhere('b.accountsOffice LIKE :data_like')
+                    ->orWhere('b.notes LIKE :data_like')
+                    ->orWhere('b.archiveNote LIKE :data_like')
+                    ->setParameter('data_like', '%' . $string . '%')
+                    ->setParameter('data_int', (int)$string)
+                    ->getQuery();
+                $businesses = $query->getResult();
             }
-//            $businessRepo = $em->getRepository('AppBundle:Business');
-//            var_dump($d);
         }
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
-                    'base_dir' => realpath($this->getParameter('kernel.project_dir')) . DIRECTORY_SEPARATOR,
-                    'form' => $form->createView(),
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')) . DIRECTORY_SEPARATOR,
+            'form' => $form->createView(),
+            'individuals' => $individuals,
+            'businesses' => $businesses,
         ]);
     }
 
